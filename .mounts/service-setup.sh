e13933a8e9f8e1135643c4b274667c47605e65d6
ben7sys <ben@7sys.de>
2024-05-09 23:52:41 +0200

    service setup beschreibung hinzugefügt und dokumentation verbessert

--- .mounts/service-setup.sh
+++ .mounts/service-setup.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 # Skript zur Einrichtung von Service-Verzeichnissen und -Dateien
-# Das Git-Repository welches die dotfiles enthält sollte nach $USER_HOME/dotfiles geklont werden
-# Wenn ein anderer Pfad verwendet wird, muss die Variable DOTFILES_DIR entsprechend angepasst werden
-# Jedes Verzeichnis und jede Datei inklusive versteckter Dateien innerhalb der Target-Verzeichnisse wird rekursiv verlinkt
-# Beispiel: ./dotfiles/.mounts/.conf wird zu ~/.mounts/.conf verlinkt
-# Anschließend werden die NFS-Mounts konfiguriert und gestartet welche in .mounts/.conf definiert sind
 
+# Anleitung:
+# 1. Das Git-Repository dotfiles nach $USER_HOME/dotfiles klonen: git clone https://github.com/ben7sys/dotfiles.git
+#    !! Wenn ein anderer Pfad verwendet wird, muss die Variable DOTFILES_DIR entsprechend angepasst werden
 # Pfadvariablen
 USER_HOME=$(eval echo ~$SUDO_USER)
 DOTFILES_DIR="$USER_HOME/dotfiles"
 TARGET_DIRS=(".config" ".mounts" ".ssh")  # Liste der zu verlinkenden Verzeichnisse
+
+# Beschreibung:
+# Dieses Skript verlinkt die Verzeichnisse .config, .mounts und .ssh aus dem dotfiles-Repository
+# in das Home-Verzeichnis des Benutzers. Anschließend wird ein systemd-Service registriert und gestartet
+# um die NFS-Mounts aus der .conf-Datei zu konfigurieren und zu starten.
+# Jedes Verzeichnis und jede Datei inklusive versteckter Dateien innerhalb der Target-Verzeichnisse wird rekursiv verlinkt
+# Beispiel: ./dotfiles/.mounts/.conf wird zu ~/.mounts/.conf verlinkt
+# Anschließend werden die NFS-Mounts konfiguriert und gestartet welche in .mounts/.conf definiert sind
 
 # Funktion, um Verzeichnisse zu verlinken
 link_directory() {
@@ -53,7 +59,8 @@ done
 # Systemd Service registrieren und starten (Beispiel für .mounts)
 if [ -f "$DOTFILES_DIR/.mounts/usermounts.service" ]; then
     echo "Registriere und starte systemd service..."
-    sudo cp "$DOTFILES_DIR/.mounts/usermounts.service" /etc/systemd/system/
+    #sudo cp "$DOTFILES_DIR/.mounts/usermounts.service" /etc/systemd/system/
+    sudo ln -s "$USER_HOME/dotfiles/.mounts/usermounts.service" /etc/systemd/system/usermounts.service
     sudo systemctl daemon-reload
     sudo systemctl enable usermounts.service
     sudo systemctl start usermounts.service
