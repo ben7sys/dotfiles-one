9badff58b380246b0eb73b36c610cf2976fd6844
ben7sys <ben@7sys.de>
2024-08-01 16:11:44 +0200

    .

--- gpu-passthrough/configs/libvirt/hooks/qemu.d/template_vm/prepare/begin/bind_vfio.sh
+++ gpu-passthrough/configs/libvirt/hooks/qemu.d/template_vm/prepare/begin/bind_vfio.sh
@@ -4,14 +4,20 @@
 source "/etc/libvirt/hooks/kvm.conf"
 
 ## Load vfio
-modprobe vfio
-modprobe vfio_iommu_type1
-modprobe vfio_pci
+## only uncomment if you do not have mkinitcpio.conf configured
+## edit /etc/mkinitcpio.conf
+## MODULES=(vfio vfio_iommu_type1 vfio_pci vfio_virqfd)
+#modprobe vfio
+#modprobe vfio_iommu_type1
+#modprobe vfio_pci
 
 ## Unbind gpu from nvidia and bind to vfio
 virsh nodedev-detach $VIRSH_GPU_VIDEO
 virsh nodedev-detach $VIRSH_GPU_AUDIO
-virsh nodedev-detach $VIRSH_GPU_USB
-virsh nodedev-detach $VIRSH_GPU_SERIAL
+
+## Uncomment the following lines if needed
+## Unbind usb and serial from nvidia and bind to vfio
+#virsh nodedev-detach $VIRSH_GPU_USB
+#virsh nodedev-detach $VIRSH_GPU_SERIAL
 ## Unbind ssd from nvme and bind to vfio
-virsh nodedev-detach $VIRSH_NVME_SSD
+#virsh nodedev-detach $VIRSH_NVME_SSD
