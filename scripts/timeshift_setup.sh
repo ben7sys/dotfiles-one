1d0b5b5f4600a1862f45da531b71d5d282319f40
ben7sys <ben@7sys.de>
2024-08-04 14:34:39 +0200

    chore: Update timeshift_setup.sh to source functions.sh

--- scripts/timeshift_setup.sh
+++ scripts/timeshift_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+log_message "timeshift_setup.sh script started" "yellow"
 ## timeshift_setup.sh: Configure Timeshift for BTRFS snapshots with a systemd service
 ## This script should be run as a normal user. It will elevate privileges only for commands that require root.
 
