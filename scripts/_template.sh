6cde8dccf98217066049fd172f559572d1669130
ben7sys <ben@7sys.de>
2024-08-04 16:22:50 +0200

    Refactor _template.sh to improve sourcing and directory handling

--- scripts/_template.sh
+++ scripts/_template.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# install_packages.sh: Install packages from YAML config for multiple package managers
+# _template.sh: A template for new scripts
 
 ## Enable debug mode if needed
 # set -x
 
-## Enable strict mode
+## Enable strict mode. (error will cause the script to stop)
 set -eo pipefail
 
-# Source the config file
+# Source the config file always. 
 source "$(dirname "$0")/config.sh"
 
-# Function
+# Functions are in the functions.sh file which is sourced in the config.sh file
+# --- START SCRIPT ---
