c3017f5dbeed10daa476c9edcd12cc6994eb923b
ben7sys <ben@7sys.de>
2024-08-05 02:59:20 +0200

    chore: Update Kvantum dependencies and installation process

--- scripts/test_desktop_functions.sh
+++ scripts/test_desktop_functions.sh
@@ -53,7 +53,10 @@ echo "Testing set_wallpaper function..."
 set_wallpaper "$DOTFILES_DIR/user/wallpaper_default.png"
 
 # Ensure Kvantum is installed using the install_packages function
-install_packages "kvantum-qt5 kvantum-qt6 kvantum-manager kvantum-theme"
+install_packages "kvantum-qt5"
+install_packages "kvantum-qt6"
+install_packages "kvantum-manager"
+install_packages "kvantum-theme"
 
 # Function to set Kvantum theme
 set_kvantum_theme() {
