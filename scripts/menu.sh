0e11496232595b66c5f349dfb99b834f8f9a9f51
ben7sys <ben@7sys.de>
2024-08-18 11:41:22 +0200

    menu.sh script created

--- scripts/menu.sh
+++ scripts/menu.sh
@@ -1,33 +1,62 @@
 #!/bin/bash
 
-## Log the start of the script
+# Log the start of the script
 log_message "Running Dotfiles: menu.sh $(date '+%Y-%m-%d %H:%M:%S')" "yellow"
 
-## Source files
+# Source necessary files
 source "config.sh"
 source "functions.sh"
 source "install_packages.sh"
 
-## Error handling
+# Error handling
 exec 2> >(while read -r line; do error_handler "$line"; done)
 
-## --- FUNCTIONS ---
+# --- CONFIGURATION ---
 
+# Define script directories
+SCRIPT_DIRS=("$DOTFILES_SCRIPTS" "$DOTFILES_DIR/gpu-passthrough/scripts")
 
-## --- MENU ---
+# Define excluded scripts
+EXCLUDED_SCRIPTS=("config.sh" "menu.sh")
 
-## Main
-main() {
+# --- FUNCTIONS ---
 
-    
-
-
-        
-
+# Function to list available scripts in the specified directories
+list_scripts() {
+    local scripts=()
+    for dir in "${SCRIPT_DIRS[@]}"; do
+        # Find all scripts in the directory
+        for script in "$dir"/*.sh; do
+            # Check if the script is in the excluded list
+            local base_script=$(basename "$script")
+            if [[ ! " ${EXCLUDED_SCRIPTS[@]} " =~ " ${base_script} " ]]; then
+                scripts+=("$script")
+            fi
+        done
+    done
+    echo "${scripts[@]}"
 }
 
+# Function to display the menu and get user selection
+show_menu() {
+    local scripts=($(list_scripts))
+    local PS3="Please select a script to execute: "
 
-# Run the main function if the script is executed directly
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
-    main
-fi
+    echo "Available scripts:"
+    select script in "${scripts[@]}"; do
+        if [[ -n "$script" ]]; then
+            echo "You selected $script"
+            bash "$script"
+            break
+        else
+            echo "Invalid selection. Please try again."
+        fi
+    done
+}
+
+# --- MENU ---
+
+ensure_correct_location # Ensure the script is run from the correct directory
+
+# Display the menu to the user
+show_menu
