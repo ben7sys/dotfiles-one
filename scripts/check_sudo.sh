888d9b62fd179252e8ff6f58b0598165eeb72118
ben7sys <ben@7sys.de>
2024-08-18 14:57:50 +0200

    Refactor set_profile_picture.sh script to set the profile picture for the currently logged-in user renamend files for structure purposes deleted check_sudo cause its in functions plan is to redesgn the handling of desktop settings

--- scripts/check_sudo.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Check if the script has sudo privileges
-check_sudo() {
-    if ! sudo -v &> /dev/null; then
-        echo "This script requires sudo privileges" 1>&2
-        exit 1
-    fi
-}
